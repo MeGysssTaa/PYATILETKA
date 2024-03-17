666401b332604979e9f828972e58cc512c25406c
German Vekhorev <darksidecode@ya.ru>
2024-03-17 20:07:31 +0200

    sdfsdf

--- app/src/main/java/su/tovarischi/pyatiletka/MainActivity.kt
+++ app/src/main/java/su/tovarischi/pyatiletka/MainActivity.kt
@@ -2,28 +2,69 @@ package su.tovarischi.pyatiletka
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        navView.setNavigationItemSelectedListener(this)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.navigationIcon = AppCompatResources.getDrawable(applicationContext, R.drawable.ic_launcher_fg)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         startService(Intent(this, BackgroundMusicService::class.java))
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_groceries -> {
+                // Handle Groceries navigation
+                println("groceries")
+            }
+            R.id.nav_work -> {
+                // Handle Work Tasks navigation
+                println("work")
+            }
+            R.id.nav_home -> {
+                // Handle Home Tasks navigation
+                println("home")
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
     override fun onPause() {
         super.onPause()
-
         val pauseMusicIntent = Intent(this, BackgroundMusicService::class.java)
         pauseMusicIntent.action = "PAUSE"
         startService(pauseMusicIntent)
@@ -31,7 +72,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-
         val resumeMusicIntent = Intent(this, BackgroundMusicService::class.java)
         resumeMusicIntent.action = "RESUME"
         startService(resumeMusicIntent)

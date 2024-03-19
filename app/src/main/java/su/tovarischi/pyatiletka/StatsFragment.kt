3d9840d737402fa8ad64f9fe0b8694f6d4da5970
German Vekhorev <darksidecode@ya.ru>
2024-03-19 14:29:31 +0200

    Remove add button from stats view

--- app/src/main/java/su/tovarischi/pyatiletka/StatsFragment.kt
+++ app/src/main/java/su/tovarischi/pyatiletka/StatsFragment.kt
@@ -19,14 +19,14 @@ class StatsFragment : Fragment(R.layout.fragment_stats) {
     private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val view = inflater.inflate(R.layout.fragment_task_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_stats, container, false)
         initRecyclerView(view)
         initStats()
         return view
     }
 
     private fun initRecyclerView(view: View) {
-        recyclerView = view.findViewById(R.id.recyclerView)
+        recyclerView = view.findViewById(R.id.statsRecyclerView)
         statsAdapter = StatsAdapter(requireContext())
         recyclerView.adapter = statsAdapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
